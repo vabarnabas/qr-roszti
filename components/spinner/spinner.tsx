37799aba901d5cad852d69958f89162560749678
vabarnabas <vabarnabas@gmail.com>
2022-04-07 22:37:00 +0200

    Updates

---
+++ components/spinner/spinner.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
+
+const Spinner = () => {
+  return (
+    <AiOutlineLoading3Quarters className="animate-spin justify-self-center text-4xl" />
+  )
+}
+
+export default Spinner
