ad3f92fb5d03dee757e33c8f74d0a733da2fbc9c
Varga Barnabas <vabarnabas@gmail.com>
2022-05-16 17:22:25 +0200

    Fixes

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router"
 import React, { useEffect, useState } from "react"
+import { FaKey, FaUserAlt } from "react-icons/fa"
 import { HiExternalLink } from "react-icons/hi"
 import { IoCube, IoSearch } from "react-icons/io5"
 import { MdDelete } from "react-icons/md"
@@ -54,18 +55,28 @@ const Users = () => {
             <div className="w-full h-min gap-x-4 gap-y-3 grid grid-cols-1">
               {data?.users_aggregate?.nodes &&
                 chainFilter(data.users_aggregate.nodes)
+                  .startGroup()
                   .filterText(searchString, true, "displayname")
+                  .filterText(searchString, true, "role")
+                  .endGroup()
                   .out()
                   .map((item: User) => (
                     <div
                       key={item.id}
                       className="group flex items-center justify-between w-full rounded-md bg-slate-50 cursor-pointer px-4 py-2 h-min"
                     >
-                      <div className="">
-                        <p className="font-semibold text-lg">
-                          {item.displayname}
-                        </p>
-                        <p className="-mt-1 text-xs">{item.email}</p>
+                      <div className="flex items-center">
+                        {item.role === "ADMIN" ? (
+                          <FaKey className="text-sm" />
+                        ) : (
+                          <FaUserAlt className="text-sm" />
+                        )}
+                        <div className="ml-4">
+                          <p className="font-semibold text-lg">
+                            {item.displayname}
+                          </p>
+                          <p className="-mt-1 text-xs">{item.email}</p>
+                        </div>
                       </div>
                       <div className="flex items-center justify-center space-x-2">
                         <MdDelete
